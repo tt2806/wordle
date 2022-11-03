a04b58aed80c12816d0d2b3914485f1c469bc4af
cynthia0890 <cynthia0890@penguin>
2022-11-03 06:48:32 -0400

    change app files, forgot the non-null operand

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import './App.css'
 
 function App() {
-  return 'Hello, world!'
+  return (
+    <div className="flex justify-center mt-4 dark:text-white text-lg">
+      Hello, world!
+    </div>
+  )
 }
 
 export default App
